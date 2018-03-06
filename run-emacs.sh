c78c0d775fa9a72d9e9fea6ee1ba247e44aa5fcc
rodrigo <rodrigo.maia.pereira@gmail.com>
2018-03-06 18:49:05 -0300

    :metal:

--- run-emacs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-emacsclient -e "(frames-on-display-list \"${DISPLAY}\")" 1>/dev/null 2>/dev/null
-if [ "$?" = "1" ]; then
-    emacsclient -c -n -a "" "$@"
-else
-    emacsclient -n "$@"
-fi
