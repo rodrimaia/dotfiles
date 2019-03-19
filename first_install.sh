5d8921f0ed3ca334f7041c21bb4b56d8a7dedb08
Rodrigo <rodrigo.maia.pereira@gmail.com>
2019-03-19 15:16:18 -0300

    :coffee:

--- first_install.sh
+++ first_install.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 ln -s ~/dotfiles/.spacemacs ~/.
 ln -s ~/dotfiles/.tmux.conf ~/.
-ln -s ~/dotfiles/.alacritty.yml ~/
-ln -s ~/dotfiles/.alacritty.yml ~/.config/alacritty/
+ln -s ~/dotfiles/alacritty.yml ~/.config/alacritty/
 ln -s ~/dotfiles/init.vim ~/.config/nvim/
 ln -s ~/dotfiles/.hyper.js ~/
-ln -s ~/dotfiles/.alacritty.yml ~/.
 ln -s ~/dotfiles/tmuxinator ~/.config/
