2863720630cc9cd803d0791d79d0fe7fa541f44e
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2022-02-07 13:24:44 -0300

    :coffee:

--- first_install.sh
+++ first_install.sh
@@ -3,7 +3,7 @@ ln -s ~/dotfiles/.spacemacs ~/.
 ln -s ~/dotfiles/.tmux/.tmux.conf ~/.
 ln -s ~/dotfiles/.tmux.conf.local ~/.
 ln -s ~/dotfiles/.alacritty.yml ~/.
-ln -s ~/dotfiles/init.vim ~/.config/nvim/
+mkdir ~/.config/nvim && ln -s ~/dotfiles/init.vim ~/.config/nvim/
 ln -s ~/dotfiles/.hyper.js ~/
 ln -s ~/dotfiles/tmuxinator ~/.config/
 ln -s ~/dotfiles/i3-config ~/.config/i3/config
